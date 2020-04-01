93ba5dc7ec4fc6add020f7cd77642b28501fb4d1
mmrozewski <mmrozews@gmail.com>
2020-04-01 17:01:48 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+    console.log("It's all gucci");
+})
+
+
+const taskButton = document.getElementById("submit");
+
+
+
+
+taskButton.addEventListener("click", () => {
+    event.preventDefault();
+    const userInput = document.getElementById("new-task-description").value;
+    if (userInput.length != 0) {
+      let currentList = document.getElementById("tasks");
+      let priority = document.getElementById("new-task-priority");
+      let priorityColor = priority.options[priority.selectedIndex].value;
+      let newTask = document.createElement("li")
+      let deleteButton = document.createElement("BUTTON");
+      deleteButton.innerText = "x";
+      newTask.innerText = userInput;
+      newTask.style.color = priorityColor;
+      newTask.appendChild(deleteButton);
+      currentList.appendChild(newTask);
+      deleteButton.addEventListener("click", () => {
+        event.preventDefault();
+        currentList.removeChild(newTask)
+      })
+      
+    }
+  });
+
+
+  }
